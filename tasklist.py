a9efcddfe5dd878f08ce156f68606e3794cebef6
mrBETAtwe <51169210+mrBETAtwe@users.noreply.github.com>
2020-11-26 13:16:16 +0700

    Update tasklist.py

--- tasklist.py
+++ tasklist.py
@@ -7,6 +7,7 @@ Base = declarative_base()
 rows = []
 
 
+# класс создающий таблицу в базе данных, в которой каждая строка является экземпляром этого класса
 class Table(Base):
     __tablename__ = 'task'
     id = Column(Integer, primary_key=True)
@@ -18,18 +19,26 @@ class Table(Base):
 
 
 class CrtList:
+    # текстовое меню
     vvod = "1) Today's tasks\n2) Week's tasks\n3) All tasks\n" \
-           "4) Missed tasks\n5) Add task\n6) Delete task\n0) Exit\n"
+           "4) Missed tasks\n5) Add task\n6) Delete task\n0) Exit\n\nSelect action: "
+    # сегоднящняя дата
     today = datetime.today()
 
+    # создание БД, в словаре self.session содержатся методы класса которые будут вызваны из текстового меню
     def __init__(self, db_name):
-        self.engine = create_engine(f'sqlite:///{db_name}.db?check_same_thread=False')
+        self.engine = create_engine(f'sqlite:///{db_name}.db')
         Base.metadata.create_all(self.engine)
         self.session = sessionmaker(bind=self.engine)()
         self.action = {'1': self.today_task, '2': self.week_task, '3': self.all_task,
                        '4': self.missed_task, '5': self.add_task, '6': self.del_task, '0': self.byye}
         self.run = True
         self.main()
+
+    # Вывод меню в цикле пока не будет выбрано действие выход.
+    # Строка self.action.get(act, lambda: None)() получает данные из ввода в меню
+    # и вызывает метод класса по ключу в словаре self.action, если ввод был ошибочным
+    # и в словаре отсутствует ключ полученный из меню, вызывается функция lambda: None
 
     def main(self):
         while self.run:
@@ -38,18 +47,24 @@ class CrtList:
             self.action.get(act, lambda: None)()
             print() if act != '0' else print() if act != '0' else print('Bye!')
 
+    # печатает задачи на сегодня
     def today_task(self):
         tasks_today = self.session.query(Table).filter(Table.deadline == self.today.date()).all()
         print(f'Today: {self.today.day} {self.today.strftime("%b")}')
         self.wrt_task(tasks_today) if tasks_today else print('Nothing to do!')
 
+    # создаёт новую задачу
     def add_task(self):
-        new_row = Table(task=input('Enter task'), deadline=datetime.strptime(input('Enter deadline'),
-                                                                             '%Y-%m-%d').date())
-        self.session.add(new_row)
-        self.session.commit()
-        print('The task has been added!')
+        try:
+            new_row = Table(task=input('Enter task: '), deadline=datetime.strptime(input
+                                                        ('Enter deadline (YYYY-mm-dd): '), '%Y-%m-%d').date())
+            self.session.add(new_row)
+            self.session.commit()
+            print('The task has been added!')
+        except ValueError:
+            print('Select action number in numbers')
 
+    # печатает задачи на ближайшие семь дней
     def week_task(self):
         for i in range(1, 8):
             task_week_day = self.today.date() + timedelta(days=i - 1)
@@ -58,22 +73,30 @@ class CrtList:
             self.wrt_task(task_day) if task_day else print('Nothing to do!')
             print()
 
+    # печатает все задачи
     def all_task(self):
         task_all = self.session.query(Table).order_by(Table.deadline).all()
         for task in task_all:
             print(f'{task}. {task.deadline.day} {task.deadline.strftime("%b")}')
 
+    # печатает задачи которые были пропушенны
     def missed_task(self):
         missed_task = self.session.query(Table).filter(Table.deadline < self.today.date()).all()
         self.wrt_task(missed_task) if missed_task else print('Nothing is missed!')
 
+    # удаляет выбранные задачи
     def del_task(self):
-        del_task = self.session.query(Table).order_by(Table.deadline).all()
-        print('Choose the number of the task you want to delete:')
-        for task in del_task:
-            print(f'{task}. {task.deadline.day} {task.deadline.strftime("%b")}')
-        self.session.delete(del_task[int(input()) - 1])
-        self.session.commit() if del_task else print('Nothing to delete')
+        try:
+            del_task = self.session.query(Table).order_by(Table.deadline).all()
+            print('Choose the number of the task you want to delete:')
+            for task in del_task:
+                print(f'{task}. {task.deadline.day} {task.deadline.strftime("%b")}')
+            self.session.delete(del_task[int(input()) - 1])
+            self.session.commit() if del_task else print('Nothing to delete')
+        except IndexError:
+            print('There is no such task')
+        except ValueError:
+            print('Enter the task number in numbers')
 
     @staticmethod
     def wrt_task(tasks):
@@ -82,5 +105,6 @@ class CrtList:
 
     def byye(self):
         self.run = False
+
 
 CrtList('todo')
